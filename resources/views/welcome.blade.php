e46f16b7a796974b2667ca026120d5558002306c
DITSOB <melonbread2248@gmail.com>
2025-02-04 13:11:14 +0545

    added sliders

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,30 @@
 @extends('Layout.master')
 @section('content')
+<div class="slide-container">
 
     <div class="slider">
-        <div class="list">
-            <div class="item">
-                <img src="{{ asset('img/SliderImage1.jpg'); }}" alt="">
-                <a href="">Get Started</a>
+        <div class="slides">
+            <input type="radio" name="radio-btn" id="radio1">
+            <input type="radio" name="radio-btn" id="radio2">
+
+            <div class="slide first">
+                <img src="{{ asset('img/SliderImage1.jpg'); }}">
             </div>
-            <div class="item">
-                <img src="{{ asset('img/SliderImage2.png'); }}" alt="">
-                <a href="">Explore</a>
+            <div class="slide">
+                <img src="{{ asset('img/SliderImage2.png'); }}">
             </div>
+
+            <div class="navigation-auto">
+                <div class="auto-btn1"></div>
+                <div class="auto-btn2"></div>
+            </div>
+        </div>
+        <div class="navigation-manual">
+            <label for="radio1" class="manual-btn"></label> 
+            <label for="radio2" class="manual-btn"></label>        
         </div>
 
-        <div class="buttons">
-            <button id="previous"><</button>
-            <button id="next">></button>
-        </div>
-
-        <div class="slider">
-            <ul class="dots">
-                <li></li>
-                <li></li>
-            </ul>
-        </div>
     </div>
+</div>
 
 @endsection
